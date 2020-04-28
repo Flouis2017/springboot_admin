7edb2d6f585dfedb08e37f3ddc44082e350fd802
Floui2017 <315587951@qq.com>
2020-04-28 17:46:48 +0800

    first commit

---
+++ src/test/java/com/flouis/SpringbootAdminApplicationTests.java
@@ -0,0 +1,12 @@
+package com.flouis;
+
+import org.junit.Test;
+
+public class SpringbootAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println(Thread.currentThread().getName());
+	}
+
+}
